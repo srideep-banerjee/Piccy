3baee199ffb4032556c79a2956e5bb0d7ecdeb38
srideep-banerjee <banerjee.srideep@gmail.com>
2023-10-27 19:36:35 +0530

    Updated HomeViewModel code

--- app/src/main/java/com/example/piccy/viewmodels/HomeViewModel.kt
+++ app/src/main/java/com/example/piccy/viewmodels/HomeViewModel.kt
@@ -4,9 +4,7 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class HomeViewMode: ViewModel() {
-    val mutableLiveData: MutableLiveData<Int> by lazy {
-        MutableLiveData<Int>(0)
-    }
+    val mutableLiveData = MutableLiveData(0)
 
     fun incrementCount() {
         mutableLiveData.value = mutableLiveData.value!! + 1
