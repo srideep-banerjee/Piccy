aa6f328f65abbaf817a5cbb395ed4e8aed53800b
srideep-banerjee <banerjee.srideep@gmail.com>
2023-12-18 19:22:19 +0530

    Added to Authenticator interface

--- app/src/main/java/com/example/piccy/model/Authenticator.java
+++ app/src/main/java/com/example/piccy/model/Authenticator.java
@@ -1,6 +1,7 @@
 package com.example.piccy.model;
 
 public interface Authenticator {
-    void signUp(String email, String password);
+    void signUp(String email, String name, String password);
     void logIn(String email, String password);
+    boolean isLoggedIn();
 }
