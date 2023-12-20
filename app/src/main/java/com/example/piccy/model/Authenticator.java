022a0a85e3bfee000186a2e85cf29ce4a5ea2920
srideep-banerjee <banerjee.srideep@gmail.com>
2023-12-20 22:19:23 +0530

    Added callback parameters to Authenticator interface

--- app/src/main/java/com/example/piccy/model/Authenticator.java
+++ app/src/main/java/com/example/piccy/model/Authenticator.java
@@ -1,7 +1,15 @@
 package com.example.piccy.model;
 
+import androidx.annotation.NonNull;
+
+import java.util.function.BiConsumer;
+import java.util.function.Consumer;
+
 public interface Authenticator {
-    void signUp(String email, String name, String password);
-    void logIn(String email, String password);
+
+    void signUp(@NonNull String email, @NonNull String name, @NonNull String password, @NonNull BiConsumer<Boolean, String> onComplete);
+
+    void logIn(@NonNull String email, @NonNull String password, @NonNull BiConsumer<Boolean, String> onComplete);
+
     boolean isLoggedIn();
 }
