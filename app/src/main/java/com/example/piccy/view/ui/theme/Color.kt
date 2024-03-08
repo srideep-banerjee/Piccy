eb21e10f583f773aa66057a0d61bdbd2c925c861
srideep-banerjee <banerjee.srideep@gmail.com>
2024-03-08 11:11:12 +0530

    Added Jetpack Compose MaterialTheme files and dependencies

---
+++ app/src/main/java/com/example/piccy/view/ui/theme/Color.kt
@@ -0,0 +1,9 @@
+package com.example.piccy.view.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple200 = Color(0xFFBB86FC)
+val Purple500 = Color(0xFF6200EE)
+val Teal200 = Color(0xFF03DAC5)
+val Black = Color.Black
+val White = Color.White
