98dd24d18c563d3f9c12c2562db484c9e0c95469
srideep-banerjee <banerjee.srideep@gmail.com>
2024-03-20 12:10:28 +0530

    Modified MainViewModel to automatically add user data on startup

--- app/src/main/java/com/example/piccy/viewmodels/MainViewModel.kt
+++ app/src/main/java/com/example/piccy/viewmodels/MainViewModel.kt
@@ -2,15 +2,50 @@ package com.example.piccy.viewmodels
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.piccy.model.auth.Authenticator
+import com.example.piccy.model.auth.FirebaseAuthenticator
+import com.example.piccy.model.auth.UserAuthenticationState
+import com.example.piccy.model.db.DbHelper
+import com.example.piccy.model.db.FirebaseDbHelper
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class MainViewModel: ViewModel() {
-    private var currentScreen = MainScreen.HOME
 
+    private val authenticator: Authenticator by lazy {
+        FirebaseAuthenticator()
+    }
+    private val dbHelper: DbHelper by lazy {
+        FirebaseDbHelper(authenticator)
+    }
+    private var userAuthenticationState = UserAuthenticationState.NONE
+
+    private var currentScreen = MainScreen.HOME
     var searchViewExpanded :MutableLiveData<Boolean> = MutableLiveData(false)
         private set
-
     var searchQueryText: MutableLiveData<String> = MutableLiveData("")
         private set
+
+    //null -> user account non existent, "" -> user account exists without pfp, "pfpid"
+    var pfp: MutableLiveData<String?> = MutableLiveData(null)
+        private set
+
+    init {
+        viewModelScope.launch(Dispatchers.IO) {
+            userAuthenticationState = authenticator.userAuthenticationState
+        }
+
+        dbHelper.checkUserAccountExists { accountExists ->
+            if (accountExists) {
+                dbHelper.getPfpId {
+                    pfp.postValue(it?:"")
+                }
+            } else {
+                dbHelper.createUserAccount {}
+            }
+        }
+    }
 
     fun updateScreen(screen: MainScreen) {
         if(screen != currentScreen) {
